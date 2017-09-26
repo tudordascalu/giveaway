709e1d0c0b9bd8f1570c4a5da73b69e2fc673633
tudor <tudorlaurentiu1@yahoo.com>
2017-09-26 13:39:07 +0200

    Added functional category selection.

--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -11,27 +11,32 @@ export class ItemsComponent implements OnInit {
     {
      'imageURL': './assets/images/carousel/carousel-1.jpg',
      'title': 'Bed',
-     'body': 'This item is a very nice bed I used since I was little' 
+     'body': 'This item is a very nice bed I used since I was little',
+     'category': 2
     },
     {
       'imageURL': './assets/images/carousel/carousel-2.jpg',
       'title': 'Drawer',
-      'body': 'This item is a very nice bed I used since I was little' 
+      'body': 'This item is a very nice bed I used since I was little',
+      'category': 2 
      },
      {
       'imageURL': './assets/images/carousel/carousel-3.jpg',
       'title': 'Clothes',
-      'body': 'This item is a very nice bed I used since I was little' 
+      'body': 'This item is a very nice bed I used since I was little',
+      'category': 4 
      },
      {
       'imageURL': './assets/images/carousel/carousel-4.jpg',
       'title': 'Lamp',
-      'body': 'This item is a very nice bed I used since I was little' 
+      'body': 'This item is a very nice bed I used since I was little',
+      'category': 3 
      },
      {
       'imageURL': './assets/images/carousel/carousel-5.jpg',
       'title': 'Speakers',
-      'body': 'This item is a very nice bed I used since I was little' 
+      'body': 'This item is a very nice bed I used since I was little',
+      'category': 3 
      }]
   constructor() { }
 
